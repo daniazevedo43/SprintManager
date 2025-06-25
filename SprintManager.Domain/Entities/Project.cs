6e83e313be1c86c948c164fa6e6c940ae8ab4085
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-06-25 18:08:01 +0100

    Project domain

---
+++ SprintManager.Domain/Entities/Project.cs
@@ -0,0 +1,63 @@
+﻿using SprintManager.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprintManager.Domain.Entities
+{
+    public class Project
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public DateTime CreationDate { get; private set; }
+        public ProjectStatus Status { get; private set; }
+
+        public Project(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Project's name can't be null.", nameof(name));
+
+            Id = Guid.NewGuid();
+            Name = name;
+            CreationDate = DateTime.Now;
+            Status = ProjectStatus.Active;
+        }
+
+        public Project(string name, string description)
+        {
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Project's name can't be null.", nameof(name));
+            if (description.Length > 500) throw new ArgumentException("Project's description can't exceed 500 characters.", nameof(description));
+
+            Id = Guid.NewGuid();
+            Name = name;
+            Description = description;
+            CreationDate = DateTime.Now;
+            Status = ProjectStatus.Active;
+        }
+
+        // Update project's name
+        public void SetName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Project's name can't be null.", nameof(name));
+            if (name.Length > 255) throw new ArgumentException("Project's name can't exceed 255 characters.", nameof(name));
+
+            Name = name;
+        }
+
+        // Update project's description
+        public void SetDescription(string description)
+        {
+            if (description.Length > 500) throw new ArgumentException("Project's description can't exceed 500 characters.", nameof(description));
+
+            Description = description;
+        }
+
+        // Update project's status
+        public void SetStatus(ProjectStatus status)
+        {
+            Status = status;
+        }
+    }
+}
