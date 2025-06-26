4b0a6f71bfa5425cc97719fe4ad81667b94141bd
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-06-26 18:39:58 +0100

    Work item domain

---
+++ SprintManager.Domain/Enums/WorkItemPriorityLevel.cs
@@ -0,0 +1,11 @@
+﻿namespace SprintManager.Domain.Enums
+{
+    public enum WorkItemPriorityLevel
+    {
+        NotSet = 0,
+        Critical = 1,
+        High = 2,
+        Medium = 3,
+        Low = 4
+    }
+}
