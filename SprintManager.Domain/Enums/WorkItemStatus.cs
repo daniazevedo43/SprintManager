772092766e610bdcf53078d525f7b8519a279adb
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-06-26 18:41:29 +0100

    WorkItem domain

---
+++ SprintManager.Domain/Enums/WorkItemStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprintManager.Domain.Enums
+{
+    public enum WorkItemStatus
+    {
+        New = 0,
+        Active = 1,
+        InReview = 2,
+        Closed = 3
+    }
+}
---
+++ SprintManager.Domain/Enums/WorkItemType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprintManager.Domain.Enums
+{
+    public enum WorkItemType
+    {
+        Task = 0,
+        Bug = 1,
+        Test = 2
+    }
+}
