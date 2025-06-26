877c0a332d82afad09a099fdb2ea627e9d86c919
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-06-26 16:53:22 +0100

    Sprint domain implemented

--- SprintManager.Domain/Entities/Sprint.cs
+++ SprintManager.Domain/Entities/Sprint.cs
@@ -9,36 +9,72 @@ namespace SprintManager.Domain.Entities
         public string Name { get; private set; }
         public DateTime StartDate { get; private set; }
         public DateTime EndDate { get; private set; }
-        public string Description { get; private set; }
+        public string? Description { get; private set; }
         public SprintStatus Status { get; private set; }
         
         public Sprint(Guid projectId, string name, DateTime startDate, DateTime endDate)
         {
-            if(projectId == Guid.Empty) throw new ArgumentNullException("This project doesn't exist", nameof(projectId));
+            if(projectId == Guid.Empty) throw new ArgumentException("This project doesn't exist", nameof(projectId));
             if(string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Project's name can't be null.", nameof(name));
             if(startDate > endDate) throw new ArgumentException("Start date can't be higher than end date", nameof(startDate));
 
             Id = Guid.NewGuid();
             ProjectId = projectId;
             Name = name;
-            StartDate = startDate;
-            EndDate = endDate;
+            StartDate = startDate.ToUniversalTime();
+            EndDate = endDate.ToUniversalTime();
             Status = SprintStatus.Planned;
         }
 
-        public Sprint(Guid projectId, string name, DateTime startDate, DateTime endDate, string description)
+        public Sprint(Guid projectId, string name, DateTime startDate, DateTime endDate, string? description)
         {
-            if (projectId == Guid.Empty) throw new ArgumentNullException("This project doesn't exist", nameof(projectId));
-            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Project's name can't be null.", nameof(name));
+            if(projectId == Guid.Empty) throw new ArgumentException("This project doesn't exist", nameof(projectId));
+            if(string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Project's name can't be null.", nameof(name));
             if (startDate > endDate) throw new ArgumentException("Start date can't be higher than end date", nameof(startDate));
 
             Id = Guid.NewGuid();
             ProjectId = projectId;
             Name = name;
-            StartDate = startDate;
-            EndDate = endDate;
+            StartDate = startDate.ToUniversalTime();
+            EndDate = endDate.ToUniversalTime();
             Description = description;
             Status = SprintStatus.Planned;
+        }
+
+        // Update sprint's name
+        public void SetName(string name)
+        {
+            if(string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Project's name can't be null.", nameof(name));
+
+            Name = name;
+        }
+
+        // Update sprint's start date
+        public void SetStartDate(DateTime startDate)
+        { 
+            if(startDate > EndDate) throw new ArgumentException("Start date can't be higher than end date", nameof(startDate));
+
+            StartDate = startDate.ToUniversalTime();
+        }
+
+        // Update sprint's end date
+        public void SetEndDate(DateTime endDate)
+        {
+            if(StartDate > endDate) throw new ArgumentException("Start date can't be higher than end date", nameof(endDate));
+
+            EndDate = endDate.ToUniversalTime();
+        }
+
+        // Update sprint's description
+        public void SetDescription(string description)
+        {
+            Description = description;
+        }
+
+        // Update sprint's status
+        public void SetStatus(SprintStatus status)
+        {
+            Status = status;
         }
     }
 }
