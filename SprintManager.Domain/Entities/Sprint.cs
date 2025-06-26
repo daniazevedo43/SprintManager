7b9b3b8b684f9ef3396e1c6f96d47a4ca7ddba6e
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-06-26 12:41:00 +0100

    Sprint domain implementation started

---
+++ SprintManager.Domain/Entities/Sprint.cs
@@ -0,0 +1,44 @@
+﻿using SprintManager.Domain.Enums;
+
+namespace SprintManager.Domain.Entities
+{
+    public class Sprint
+    {
+        public Guid Id { get; private set; }
+        public Guid ProjectId { get; private set; }
+        public string Name { get; private set; }
+        public DateTime StartDate { get; private set; }
+        public DateTime EndDate { get; private set; }
+        public string Description { get; private set; }
+        public SprintStatus Status { get; private set; }
+        
+        public Sprint(Guid projectId, string name, DateTime startDate, DateTime endDate)
+        {
+            if(projectId == Guid.Empty) throw new ArgumentNullException("This project doesn't exist", nameof(projectId));
+            if(string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Project's name can't be null.", nameof(name));
+            if(startDate > endDate) throw new ArgumentException("Start date can't be higher than end date", nameof(startDate));
+
+            Id = Guid.NewGuid();
+            ProjectId = projectId;
+            Name = name;
+            StartDate = startDate;
+            EndDate = endDate;
+            Status = SprintStatus.Planned;
+        }
+
+        public Sprint(Guid projectId, string name, DateTime startDate, DateTime endDate, string description)
+        {
+            if (projectId == Guid.Empty) throw new ArgumentNullException("This project doesn't exist", nameof(projectId));
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Project's name can't be null.", nameof(name));
+            if (startDate > endDate) throw new ArgumentException("Start date can't be higher than end date", nameof(startDate));
+
+            Id = Guid.NewGuid();
+            ProjectId = projectId;
+            Name = name;
+            StartDate = startDate;
+            EndDate = endDate;
+            Description = description;
+            Status = SprintStatus.Planned;
+        }
+    }
+}
---
+++ SprintManager.Domain/Enums/SprintStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprintManager.Domain.Enums
+{
+    public enum SprintStatus
+    {
+        Planned = 0,
+        Active = 1,
+        Completed = 2
+    }
+}
