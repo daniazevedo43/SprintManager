76b539bea3096eb40a52d1bb970d84b7401d9416
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-06-27 16:07:46 +0100

    Comment domain

---
+++ SprintManager.Domain/Entities/Comment.cs
@@ -0,0 +1,38 @@
+﻿namespace SprintManager.Domain.Entities
+{
+    public class Comment
+    {
+        public Guid Id { get; private set; }
+        public Guid WorkItemId { get; private set; }
+        public Guid UserId { get; private set; }
+        public string Text { get; private set; }
+        public DateTime CreationDate { get; private set; }
+
+        public Comment()
+        {
+
+        }
+
+        public Comment(Guid workItemId, Guid userId, string text)
+        {
+            if(workItemId == Guid.Empty) throw new ArgumentException("This work item doesn't exist.", nameof(workItemId));
+            if(userId == Guid.Empty) throw new ArgumentException("This user doesn't exist.", nameof(userId));
+            if(string.IsNullOrWhiteSpace(text)) throw new ArgumentException("A comment can't be null or empty", nameof(text));
+            if(text.Length > 500) throw new ArgumentException("A comment can't exceed 500 characters.", nameof(text));
+
+            Id = Guid.NewGuid();
+            WorkItemId = workItemId;
+            UserId = userId;
+            Text = text;
+            CreationDate = DateTime.UtcNow;
+        }
+
+        public void SetText(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text)) throw new ArgumentException("A comment can't be null or empty", nameof(text));
+            if (text.Length > 500) throw new ArgumentException("A comment can't exceed 500 characters.", nameof(text));
+
+            Text = text;
+        }
+    }
+}
