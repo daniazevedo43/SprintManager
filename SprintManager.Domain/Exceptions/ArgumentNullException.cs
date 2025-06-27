75ff16cf9ae74ad92e4e38cf90fcf38e70424d5b
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-06-27 17:16:28 +0100

    Exceptions for null values and out of range values

---
+++ SprintManager.Domain/Exceptions/ArgumentNullException.cs
@@ -0,0 +1,20 @@
+﻿namespace SprintManager.Domain.Exceptions
+{
+    public class ArgumentNullException : Exception
+    {
+        public ArgumentNullException() 
+        { 
+        
+        }
+
+        public ArgumentNullException(string message)
+            : base(message)
+        {
+        }
+
+        public ArgumentNullException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ SprintManager.Domain/Exceptions/ArgumentOutOfRangeException.cs
@@ -0,0 +1,39 @@
+﻿namespace SprintManager.Domain.Exceptions
+{
+    public class ArgumentOutOfRangeException : Exception
+    {
+        public int? MaxLimit { get; }
+        public int? MinLimit { get; } 
+        public int ActualLength { get; }
+
+        public ArgumentOutOfRangeException()
+        {
+        }
+
+        public ArgumentOutOfRangeException(string message)
+            : base(message)
+        {
+        }
+
+        public ArgumentOutOfRangeException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public ArgumentOutOfRangeException(string message, int? minLimit, int? maxLimit, int actualLength)
+            : base(message)
+        {
+            MinLimit = minLimit;
+            MaxLimit = maxLimit;
+            ActualLength = actualLength;
+        }
+
+        public ArgumentOutOfRangeException(string message, int? minLimit, int? maxLimit, int actualLength, Exception innerException)
+            : base(message, innerException)
+        {
+            MinLimit = minLimit;
+            MaxLimit = maxLimit;
+            ActualLength = actualLength;
+        }
+    }
+}
