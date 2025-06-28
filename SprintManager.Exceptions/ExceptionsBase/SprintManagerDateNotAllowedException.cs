6dafcee744c7f84e36fe679178499b261161a232
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-06-28 19:05:02 +0100

    Exception for invalid completion date

---
+++ SprintManager.Exceptions/ExceptionsBase/SprintManagerDateNotAllowedException.cs
@@ -0,0 +1,28 @@
+﻿namespace SprintManager.Exceptions.ExceptionsBase
+{
+    public class SprintManagerDateNotAllowedException : Exception
+    {
+        public DateTime InvalidDate { get; }
+
+        public SprintManagerDateNotAllowedException()
+        {
+
+        }
+
+        public SprintManagerDateNotAllowedException(string message)
+            : base(message)
+        {
+        }
+
+        public SprintManagerDateNotAllowedException(string message, string paramName)
+            : base(message)
+        {
+        }
+
+        public SprintManagerDateNotAllowedException(string message, DateTime invalidDate, string paramName)
+            : base(message)
+        {
+            InvalidDate = invalidDate;
+        }
+    }
+}
