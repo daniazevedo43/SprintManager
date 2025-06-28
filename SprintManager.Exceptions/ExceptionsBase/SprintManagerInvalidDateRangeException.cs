493fd228beb14c4ba28323fb4bbb89b7c02ecdbf
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-06-28 17:37:24 +0100

    Exception for invalid date range

---
+++ SprintManager.Exceptions/ExceptionsBase/SprintManagerInvalidDateRangeException.cs
@@ -0,0 +1,37 @@
+﻿namespace SprintManager.Exceptions.ExceptionsBase
+{
+    public class SprintManagerInvalidDateRangeException : Exception
+    {
+        public DateTime StartDate { get; }
+        public DateTime EndDate { get; }
+
+        public SprintManagerInvalidDateRangeException()
+        {
+
+        }
+
+        public SprintManagerInvalidDateRangeException(string message)
+            : base(message)
+        {
+        }
+
+        public SprintManagerInvalidDateRangeException(string message, string paramName)
+            : base(message)
+        {
+        }
+
+        public SprintManagerInvalidDateRangeException(string message, DateTime startDate, DateTime endDate)
+            : base(message)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+        public SprintManagerInvalidDateRangeException(string message, DateTime startDate, DateTime endDate, string paramName)
+            : base(message)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+    }
+}
