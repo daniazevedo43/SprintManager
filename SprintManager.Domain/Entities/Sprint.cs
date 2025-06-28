1fa31093a7f8260b1d5b7b469558e391ad921afa
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-06-28 17:53:44 +0100

    Sprint domain with custom exception

--- SprintManager.Domain/Entities/Sprint.cs
+++ SprintManager.Domain/Entities/Sprint.cs
@@ -1,4 +1,5 @@
 ﻿using SprintManager.Domain.Enums;
+using SprintManager.Exceptions.ExceptionsBase;
 
 namespace SprintManager.Domain.Entities
 {
@@ -19,11 +20,11 @@ namespace SprintManager.Domain.Entities
         
         public Sprint(Guid projectId, string name, DateTime startDate, DateTime endDate)
         {
-            if(projectId == Guid.Empty) throw new ArgumentException("Project can't be null or empty.", nameof(projectId));
-            if(string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Project's name can't be null or empty.", nameof(name));
-            if(startDate > endDate) throw new ArgumentException("Start date can't be higher than end date", nameof(startDate));
+            if(projectId == Guid.Empty) throw new ArgumentException("Project ID can't be null or empty.", nameof(projectId));
+            if(string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Sprint's name can't be null or empty.", nameof(name));
+            if(startDate > endDate) throw new SprintManagerInvalidDateRangeException("Start date can't be higher than end date", startDate, endDate, nameof(startDate));
 
-            if(name.Length > 255) throw new ArgumentException("Sprint's name can't exceed 255 characters.", nameof(name));
+            if(name.Length > 255) throw new SprintManagerTooLongException("Sprint's name can't exceed 255 characters.", 255, name.Length, nameof(name));
 
             Id = Guid.NewGuid();
             ProjectId = projectId;
@@ -35,12 +36,12 @@ namespace SprintManager.Domain.Entities
 
         public Sprint(Guid projectId, string name, DateTime startDate, DateTime endDate, string? description)
         {
-            if(projectId == Guid.Empty) throw new ArgumentException("This project doesn't exist", nameof(projectId));
-            if(string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Project's name can't be null.", nameof(name));
-            if(startDate > endDate) throw new ArgumentException("Start date can't be higher than end date", nameof(startDate));
+            if(projectId == Guid.Empty) throw new ArgumentException("Project ID can't be null or empty.", nameof(projectId));
+            if(string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Sprint's name can't be null or empty.", nameof(name));
+            if(startDate > endDate) throw new SprintManagerInvalidDateRangeException("Start date can't be higher than end date", startDate, endDate, nameof(startDate));
 
-            if(name.Length > 255) throw new ArgumentException("Sprint's name can't exceed 255 characters.", nameof(name));
-            if(description?.Length > 500) throw new ArgumentException("Sprint's description can't exceed 500 characters.", nameof(description));
+            if(name.Length > 255) throw new SprintManagerTooLongException("Sprint's name can't exceed 255 characters.", 255, name.Length, nameof(name));
+            if(description?.Length > 500) throw new SprintManagerTooLongException("Sprint's description can't exceed 500 characters.", 500, description.Length, nameof(description));
 
             Id = Guid.NewGuid();
             ProjectId = projectId;
@@ -54,16 +55,16 @@ namespace SprintManager.Domain.Entities
         // Update sprint's name
         public void SetName(string name)
         {
-            if(string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Project's name can't be null.", nameof(name));
-            if (name.Length > 255) throw new ArgumentException("Sprint's name can't exceed 255 characters.", nameof(name));
+            if(string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Sprint's name can't be null or empty.", nameof(name));
+            if(name.Length > 255) throw new SprintManagerTooLongException("Sprint's name can't exceed 255 characters.", 255, name.Length, nameof(name));
 
             Name = name;
         }
 
         // Update sprint's start date
         public void SetStartDate(DateTime startDate)
-        { 
-            if(startDate > EndDate) throw new ArgumentException("Start date can't be higher than end date", nameof(startDate));
+        {
+            if(startDate > EndDate) throw new SprintManagerInvalidDateRangeException("Start date can't be higher than end date", startDate, EndDate, nameof(startDate));
 
             StartDate = startDate.ToUniversalTime();
         }
@@ -71,7 +72,7 @@ namespace SprintManager.Domain.Entities
         // Update sprint's end date
         public void SetEndDate(DateTime endDate)
         {
-            if(StartDate > endDate) throw new ArgumentException("Start date can't be higher than end date", nameof(endDate));
+            if(StartDate > endDate) throw new SprintManagerInvalidDateRangeException("Start date can't be higher than end date", StartDate, endDate, nameof(endDate));
 
             EndDate = endDate.ToUniversalTime();
         }
@@ -79,7 +80,7 @@ namespace SprintManager.Domain.Entities
         // Update sprint's description
         public void SetDescription(string description)
         {
-            if (description?.Length > 500) throw new ArgumentException("Sprint's description can't exceed 500 characters.", nameof(description));
+            if (description?.Length > 500) throw new SprintManagerTooLongException("Sprint's description can't exceed 500 characters.", 500, description.Length, nameof(description));
 
             Description = description;
         }
