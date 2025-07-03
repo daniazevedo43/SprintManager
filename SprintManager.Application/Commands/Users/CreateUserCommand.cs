28155957529d140f75fb085da077cd82a7f2943b
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-07-03 12:08:57 +0100

    User command and handler created

---
+++ SprintManager.Application/Commands/Users/CreateUserCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using SprintManager.Domain.Entities;
+
+namespace SprintManager.Application.Commands.Users
+{
+    public class CreateUserCommand : IRequest<Guid>
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
