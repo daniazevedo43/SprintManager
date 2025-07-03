debd617dd28079bd52757f66f65957ac9749be27
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-07-03 17:51:06 +0100

    User repository created

---
+++ SprintManager.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using SprintManager.Application.Interfaces;
+using SprintManager.Domain.Entities;
+using SprintManager.Infrastructure.Data;
+
+namespace SprintManager.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task<User> GetByEmailAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
