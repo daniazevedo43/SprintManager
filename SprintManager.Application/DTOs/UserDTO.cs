5e8f3e475a4c2cef5441e7d964be62f057c8fa35
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-07-03 13:56:10 +0100

    Query and handler to get user by id

---
+++ SprintManager.Application/DTOs/UserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace SprintManager.Application.DTOs
+{
+    public class UserDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
