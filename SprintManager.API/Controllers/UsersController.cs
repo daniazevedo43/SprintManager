066ae9ff8188bbeb7f61fb13cc3a1fdcf342bd5c
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-07-04 18:40:54 +0100

    Endpoint to find a user

---
+++ SprintManager.API/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SprintManager.Application.DTOs;
+using SprintManager.Application.Queries.Users;
+using SprintManager.Exceptions.ExceptionsBase;
+
+namespace SprintManager.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(UserDTO), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            try
+            {
+                var result = await _mediator.Send(new GetUserByIdQuery { Id = id });
+
+                return Ok(result);
+            }
+            catch(SprintManagerNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+    }
+}
