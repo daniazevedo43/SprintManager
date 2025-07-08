d3f36cef65601e3b691b99e6b71ab1d7b8c12285
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-07-08 17:43:25 +0100

    Create user endpoint created

--- SprintManager.API/Controllers/UsersController.cs
+++ SprintManager.API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using SprintManager.Application.Commands.Users;
 using SprintManager.Application.DTOs;
 using SprintManager.Application.Queries.Users;
 using SprintManager.Exceptions.ExceptionsBase;
@@ -20,9 +21,20 @@ namespace SprintManager.API.Controllers
         [HttpGet("{id}")]
         [ProducesResponseType(typeof(UserDTO), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> GetById(Guid id)
+        public async Task<IActionResult> GetUserById(Guid id)
         {
             var result = await _mediator.Send(new GetUserByIdQuery { Id = id });
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(UserDTO), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        public async Task<IActionResult> CreateUser(CreateUserCommand command)
+        {
+            var result = await _mediator.Send(command);
 
             return Ok(result);
         }
