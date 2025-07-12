d2fa8119734b2d172665cf4fc2b4983cfdd7e403
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-07-12 16:59:18 +0100

    Get all users functionality and unit test

---
+++ SprintManager.Application/Handlers/Users/GetAllUsersHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using SprintManager.Application.DTOs;
+using SprintManager.Application.Interfaces;
+using SprintManager.Application.Queries.Users;
+
+namespace SprintManager.Application.Handlers.Users
+{
+    public class GetAllUsersHandler : IRequestHandler<GetAllUsersQuery, List<UserDTO>>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllUsersHandler(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<UserDTO>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
+        {
+            var users = await _userRepository.GetAllAsync();
+
+            return _mapper.Map<List<UserDTO>>(users);
+        }
+    }
+}
--- SprintManager.Application/Interfaces/IUserRepository.cs
+++ SprintManager.Application/Interfaces/IUserRepository.cs
@@ -4,8 +4,9 @@ namespace SprintManager.Application.Interfaces
 {
     public interface IUserRepository
     {
-        Task AddAsync(User user);
+        Task<List<User>> GetAllAsync();
         Task<User?> GetByIdAsync(Guid id);
         Task<User?> GetByEmailAsync(string email);
+        Task AddAsync(User user);
     }
 }
