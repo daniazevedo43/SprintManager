5625f9fe5d3fad0aa3cbf3967875894686c77cb2
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-07-14 19:33:47 +0100

    Delete user handler implemented

---
+++ SprintManager.Application/Commands/Users/DeleteUserCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace SprintManager.Application.Commands.Users
+{
+    public class DeleteUserCommand : IRequest<bool>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ SprintManager.Application/Handlers/Users/DeleteUserHandler.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using MediatR;
+using SprintManager.Application.Commands.Users;
+using SprintManager.Application.DTOs;
+using SprintManager.Application.Interfaces;
+using SprintManager.Exceptions.ExceptionsBase;
+
+namespace SprintManager.Application.Handlers.Users
+{
+    public class DeleteUserHandler : IRequestHandler<DeleteUserCommand, bool>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteUserHandler(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<bool> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            if(request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            var user = await _userRepository.GetByIdAsync(request.Id);
+
+            if (user == null)
+            {
+                throw new SprintManagerNotFoundException($"User with ID {request?.Id} not found");
+            }
+
+            await _userRepository.DeleteAsync(user);
+
+            return true;
+        }
+    }
+}
