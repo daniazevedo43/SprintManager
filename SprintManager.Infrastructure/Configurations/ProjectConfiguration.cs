08bf577a13f5c6fa7fb6ca0a8cca9179cfc749c6
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-07-15 16:48:25 +0100

    New table "Projects" added to database

---
+++ SprintManager.Infrastructure/Configurations/ProjectConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SprintManager.Domain.Entities;
+
+namespace SprintManager.Infrastructure.Configurations
+{
+    public class ProjectConfiguration : IEntityTypeConfiguration<Project>
+    {
+        public void Configure(EntityTypeBuilder<Project> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Name)
+                   .IsRequired()
+                   .HasMaxLength(255);
+
+            builder.Property(p => p.Description)
+                .HasMaxLength(500);
+
+            builder.Property(p => p.CreationDate)
+                .IsRequired();
+
+            builder.Property(p => p.Status)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+    }
+}
---
+++ SprintManager.Infrastructure/Migrations/20250715154519_AddProjectsTable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SprintManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProjectsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<int>(type: "int", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Projects");
+        }
+    }
+}
