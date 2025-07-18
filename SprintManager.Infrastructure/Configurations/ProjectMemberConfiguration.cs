9499ae9e9b51f31a6fac3f2853e1c8f8f205d468
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-07-18 12:58:39 +0100

    ProjectMembers table created

---
+++ SprintManager.Infrastructure/Configurations/ProjectMemberConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SprintManager.Domain.Entities;
+
+namespace SprintManager.Infrastructure.Configurations
+{
+    public class ProjectMemberConfiguration : IEntityTypeConfiguration<ProjectMember>
+    {
+        public void Configure(EntityTypeBuilder<ProjectMember> builder)
+        {
+            builder.HasKey(pm => pm.Id);
+
+            builder.Property(pm => pm.ProjectId)
+                .IsRequired();
+
+            builder.Property(pm => pm.UserId)
+                .IsRequired();
+
+            builder.Property(pm => pm.Role)
+                .IsRequired();
+        }
+    }
+}
---
+++ SprintManager.Infrastructure/Migrations/20250718114235_AddProjectMembersTable.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SprintManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProjectMembersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProjectMembers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProjectId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Role = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectMembers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProjectMembers_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProjectMembers_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectMembers_ProjectId",
+                table: "ProjectMembers",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectMembers_UserId",
+                table: "ProjectMembers",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProjectMembers");
+        }
+    }
+}
