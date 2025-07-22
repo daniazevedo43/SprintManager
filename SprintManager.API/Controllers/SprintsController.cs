20f10fca0ace37a3dd1672af834216670027cc5b
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-07-22 18:04:07 +0100

    Get all sprints endpoint working

---
+++ SprintManager.API/Controllers/SprintsController.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SprintManager.Application.DTOs;
+using SprintManager.Application.Queries.Sprints;
+
+namespace SprintManager.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SprintsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public SprintsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<SprintDTO>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAllSprints()
+        {
+            var result = await _mediator.Send(new GetAllSprintsQuery());
+
+            return Ok(result);
+        }
+    }
+}
