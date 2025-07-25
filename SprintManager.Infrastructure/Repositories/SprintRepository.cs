ac7cf1419c9001da2dcaba732b13555c165a6e21
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-07-25 13:28:44 +0100

    Revert last test commit

--- SprintManager.Infrastructure/Repositories/SprintRepository.cs
+++ SprintManager.Infrastructure/Repositories/SprintRepository.cs
@@ -31,7 +31,6 @@ namespace SprintManager.Infrastructure.Repositories
                 .FirstOrDefaultAsync(s => s.Id == id);
         }
 
-        //COMMIT TEST
         public async Task<Sprint?> GetByProjectIdAndSprintNameAsync(Guid projectId, string sprintName)
         {
             return await _context.Sprints
