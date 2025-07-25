5b54d52e31499d988ce9f2d13262bd848da7c6ca
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-07-25 16:57:43 +0100

    Delete sprint handler

---
+++ SprintManager.Application/Handlers/Sprints/DeleteSprintHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using SprintManager.Application.Commands.Sprints;
+using SprintManager.Application.Interfaces;
+using SprintManager.Exceptions.ExceptionsBase;
+
+namespace SprintManager.Application.Handlers.Sprints
+{
+    public class DeleteSprintHandler : IRequestHandler<DeleteSprintCommand>
+    {
+        private readonly ISprintRepository _sprintRepository;
+
+        public DeleteSprintHandler(ISprintRepository sprintRepository)
+        {
+            _sprintRepository = sprintRepository;
+        }
+
+        public async Task Handle(DeleteSprintCommand request, CancellationToken cancellationToken)
+        {
+            var sprint = await _sprintRepository.GetByIdAsync(request.Id);
+
+            if (sprint == null) throw new SprintManagerNotFoundException($"Sprint with ID {request.Id} not found.");
+
+            await _sprintRepository.DeleteAsync(sprint);
+        }
+    }
+}
