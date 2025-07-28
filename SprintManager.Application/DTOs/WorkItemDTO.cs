2ec768465af6451eb3e5f9d366552fee5fa5c79a
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-07-28 16:12:53 +0100

    Work item DTO created

---
+++ SprintManager.Application/DTOs/WorkItemDTO.cs
@@ -0,0 +1,23 @@
+﻿using SprintManager.Domain.Enums;
+
+namespace SprintManager.Application.DTOs
+{
+    public class WorkItemDTO
+    {
+        public Guid Id { get; set; }
+        public Guid ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public Guid? SprintId { get; set; }
+        public string? SprintName { get; set; }
+        public Guid? UserId { get; set; }
+        public string? UserName { get; set; }
+        public WorkItemType WorkItemType { get; set; }
+        public string WorkItemTitle { get; set; }
+        public string? Description { get; set; }
+        public WorkItemStatus Status { get; set; }
+        public WorkItemPriorityLevel? PriorityLevel { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? CompletionDate { get; set; }
+        public int? HoursEstimate { get; set; }
+    }
+}
