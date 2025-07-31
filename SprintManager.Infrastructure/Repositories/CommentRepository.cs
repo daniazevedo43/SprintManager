304a57db422ee5405e59ba004c9364d55297030a
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-07-31 17:03:29 +0100

    Comment repository created

---
+++ SprintManager.Infrastructure/Repositories/CommentRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SprintManager.Application.Interfaces;
+using SprintManager.Domain.Entities;
+using SprintManager.Infrastructure.Data;
+
+namespace SprintManager.Infrastructure.Repositories
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CommentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Comment>> GetAllAsync()
+        {
+            return await _context.Comments
+                .OrderBy(c => c.WorkItem)
+                .OrderByDescending(c => c.CreationDate)
+                .ToListAsync();
+        }
+    }
+}
