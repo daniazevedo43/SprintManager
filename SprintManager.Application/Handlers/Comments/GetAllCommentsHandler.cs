4b51149a5ae2d8f118aa79353f2f1467e9937d08
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-07-31 17:04:56 +0100

    Get all comments query and handler

---
+++ SprintManager.Application/Handlers/Comments/GetAllCommentsHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using SprintManager.Application.DTOs;
+using SprintManager.Application.Interfaces;
+using SprintManager.Application.Queries.Comments;
+
+namespace SprintManager.Application.Handlers.Comments
+{
+    public class GetAllCommentsHandler : IRequestHandler<GetAllCommentsQuery, List<CommentDTO>>
+    {
+        private readonly ICommentRepository _commentRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllCommentsHandler(ICommentRepository commentRepository, IMapper mapper)
+        {
+            _commentRepository = commentRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<CommentDTO>> Handle(GetAllCommentsQuery request, CancellationToken cancellationToken)
+        {
+            var comments = await _commentRepository.GetAllAsync();
+
+            return _mapper.Map<List<CommentDTO>>(comments);
+        }
+    }
+}
