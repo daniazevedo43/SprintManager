1eff5d05dea82a5fdd5533e085f035fdc71f0e64
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-08-01 12:19:55 +0100

    Get comment by id query and handler

---
+++ SprintManager.Application/Queries/Comments/GetCommentByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using SprintManager.Application.DTOs;
+
+namespace SprintManager.Application.Queries.Comments
+{
+    public class GetCommentByIdQuery : IRequest<CommentDTO>
+    {
+        public Guid Id { get; set; }
+    }
+}
--- SprintManager.Infrastructure/Repositories/CommentRepository.cs
+++ SprintManager.Infrastructure/Repositories/CommentRepository.cs
@@ -23,5 +23,13 @@ namespace SprintManager.Infrastructure.Repositories
                 .ThenByDescending(c => c.CreationDate)
                 .ToListAsync();
         }
+
+        public async Task<Comment?> GetByIdAsync(Guid id)
+        {
+            return await _context.Comments
+                .Include(c => c.WorkItem)
+                .Include(c => c.User)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
     }
 }
