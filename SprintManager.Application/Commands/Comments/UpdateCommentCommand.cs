fca5c3d55991c2c0045e004bcda695cf36b53c7f
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-08-04 16:39:49 +0100

    Update comment command and handler

---
+++ SprintManager.Application/Commands/Comments/UpdateCommentCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using SprintManager.Application.DTOs;
+
+namespace SprintManager.Application.Commands.Comments
+{
+    public class UpdateCommentCommand : IRequest<CommentDTO>
+    {
+        public Guid Id { get; set; }
+        public Guid WorkItemId { get; set; }
+        public Guid UserId { get; set; }
+        public string Text { get; set; }
+    }
+}
--- SprintManager.Infrastructure/Repositories/CommentRepository.cs
+++ SprintManager.Infrastructure/Repositories/CommentRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SprintManager.Application.Interfaces;
 using SprintManager.Domain.Entities;
+using SprintManager.Exceptions.ExceptionsBase;
 using SprintManager.Infrastructure.Data;
 
 namespace SprintManager.Infrastructure.Repositories
@@ -35,6 +36,22 @@ namespace SprintManager.Infrastructure.Repositories
         public async Task AddAsync(Comment comment)
         {
             await _context.Comments.AddAsync(comment);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Comment? comment)
+        {
+            var workItem = await _context.WorkItems.FindAsync(comment?.WorkItemId);
+            var user = await _context.Users.FindAsync(comment?.UserId);
+
+            if (comment != null)
+            {
+                if (!string.IsNullOrWhiteSpace(comment.WorkItemId.ToString()) && workItem == null) throw new SprintManagerNotFoundException($"Work item with ID {comment?.WorkItemId} not found.");
+                if (!string.IsNullOrWhiteSpace(comment.UserId.ToString()) && user == null) throw new SprintManagerNotFoundException($"User with ID {comment?.UserId} not found.");
+
+                _context.Comments.Update(comment);
+            }
+
             await _context.SaveChangesAsync();
         }
     }
