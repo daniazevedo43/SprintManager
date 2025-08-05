9823aa5491887232b46307f2031be9f8381be3b6
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-08-05 16:12:49 +0100

    Show update date property in comments endpoint

--- SprintManager.Application/DTOs/CommentDTO.cs
+++ SprintManager.Application/DTOs/CommentDTO.cs
@@ -9,5 +9,6 @@
         public string UserName { get; set; }
         public string Text { get; set; }
         public DateTime CreationDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
     }
 }
