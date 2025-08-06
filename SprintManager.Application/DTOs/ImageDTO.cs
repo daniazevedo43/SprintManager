88f2e053e4683f84e58b96990511a59ae26e3401
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-08-06 16:53:48 +0100

    ImageDTO created

---
+++ SprintManager.Application/DTOs/ImageDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace SprintManager.Application.DTOs
+{
+    public class ImageDTO
+    {
+        public Guid Id { get; set; }
+        public Guid WorkItemId { get; set; }
+        public Guid UserId { get; set; }
+        public string ContentType { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public DateTime AttachmentDate { get; set; }
+    }
+}
