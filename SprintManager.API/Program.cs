5c40564387ccad727ca284ead2d61b5ad69f9450
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-08-06 18:11:25 +0100

    Dependency injection for image repository

--- SprintManager.API/Program.cs
+++ SprintManager.API/Program.cs
@@ -30,6 +30,7 @@ builder.Services.AddScoped<IProjectMemberRepository, ProjectMemberRepository>();
 builder.Services.AddScoped<ISprintRepository, SprintRepository>();
 builder.Services.AddScoped<IWorkItemRepository, WorkItemRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<IImageRepository, ImageRepository>();
 
 builder.Services.AddControllers().AddJsonOptions(x =>
 {
