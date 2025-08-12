574dcf439de9da5a6b7955df1b389f9dab318839
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-08-12 17:41:36 +0100

    Test "SprintManagerFileNowAllowed" exception throwing

--- SprintManager.Domain.Tests/ImageTests.cs
+++ SprintManager.Domain.Tests/ImageTests.cs
@@ -1,4 +1,5 @@
 ﻿using SprintManager.Domain.Entities;
+using SprintManager.Exceptions.ExceptionsBase;
 
 namespace SprintManager.Domain.Tests
 {
@@ -10,13 +11,13 @@ namespace SprintManager.Domain.Tests
         {
             Guid workItemId = Guid.NewGuid();
             Guid attachedByUserId = Guid.NewGuid();
-            var image = new Image(workItemId, attachedByUserId, "image/jpeg", "image", "path");
+            var image = new Image(workItemId, attachedByUserId, "image/jpeg", "image.jpeg", "path");
 
             Assert.NotEqual(Guid.Empty, image.Id);
             Assert.Equal(workItemId, image.WorkItemId);
             Assert.Equal(attachedByUserId, image.UserId);
             Assert.Equal("image/jpeg", image.ContentType);
-            Assert.Equal("image", image.FileName);
+            Assert.Equal("image.jpeg", image.FileName);
             Assert.Equal("path", image.FilePath);
             Assert.Equal(DateTime.UtcNow.Date, image.AttachmentDate.Date);
         }
@@ -26,7 +27,7 @@ namespace SprintManager.Domain.Tests
         public void VerifyWorkItemId_ThrowsException_WhenWorkItemIsNullOrEmpty()
         {
             var exception = Assert.Throws<ArgumentNullException>(() =>
-                new Image(Guid.Empty, Guid.NewGuid(), "image/jpeg", "image", "path")
+                new Image(Guid.Empty, Guid.NewGuid(), "image/jpeg", "image.jpeg", "path")
             );
 
             Assert.Equal("Work item ID can't be null or empty. (Parameter 'workItemId')", exception.Message);
@@ -34,13 +35,29 @@ namespace SprintManager.Domain.Tests
 
         // Test exception throwing when user ID is null or empty
         [Fact]
-        public void VerifyAttachedByUserId_ThrowsException_WhenAttachedByUserIdIsNullOrEmpty()
+        public void VerifyUserId_ThrowsException_WhenAttachedByUserIdIsNullOrEmpty()
         {
             var exception = Assert.Throws<ArgumentNullException>(() =>
-                new Image(Guid.NewGuid(), Guid.Empty, "image/jpeg", "image", "path")
+                new Image(Guid.NewGuid(), Guid.Empty, "image/jpeg", "image.jpeg", "path")
             );
 
             Assert.Equal("User ID can't be null or empty. (Parameter 'userId')", exception.Message);
+        }
+
+        // Test exception throwing when file extension is not allowed
+        [Theory]
+        [InlineData(".pdf")]
+        [InlineData(".txt")]
+        [InlineData(".svg")]
+        public void VerifyContentType_ThrowsException_WhenFileIsNotAllowed(string extension)
+        {
+            var allowedExtensions = new[] { ".png", ".jpg", ".jpeg" };
+
+            var exception = Assert.Throws<SprintManagerFileNotAllowedException>(() =>
+                new Image(Guid.NewGuid(), Guid.NewGuid(), $"image/{extension}", $"image.{extension}", "path")
+            );
+
+            Assert.Equal($"File extension not allowed. Please upload a file with the following extensions: {string.Join(", ", allowedExtensions)}", exception.Message);
         }
     }
 }
