f1a22e4a14eaa3279abe5e4b65a720c443288e45
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-08-20 16:12:30 +0100

    Delete image command and handler

---
+++ SprintManager.Application/Commands/Images/DeleteImageCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace SprintManager.Application.Commands.Images
+{
+    public class DeleteImageCommand : IRequest
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ SprintManager.Application/Handlers/Images/DeleteImageHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using SprintManager.Application.Commands.Images;
+using SprintManager.Application.Interfaces;
+using SprintManager.Exceptions.ExceptionsBase;
+
+namespace SprintManager.Application.Handlers.Images
+{
+    public class DeleteImageHandler : IRequestHandler<DeleteImageCommand>
+    {
+        private readonly IImageRepository _imageRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteImageHandler (IImageRepository imageRepository, IMapper mapper)
+        {
+            _imageRepository = imageRepository;
+            _mapper = mapper;
+        }
+
+
+        public async Task Handle(DeleteImageCommand request, CancellationToken cancellationToken)
+        {
+            var image = await _imageRepository.GetByIdAsync(request.Id);
+
+            if (image == null) throw new SprintManagerNotFoundException($"Image with ID {request?.Id} not found.");
+        
+            await _imageRepository.DeleteAsync(image);
+        }
+    }
+}
