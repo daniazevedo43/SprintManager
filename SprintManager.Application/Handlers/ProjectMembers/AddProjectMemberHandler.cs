7230084c30897b1a204073f245d27dee37c7ea95
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-08-22 15:37:17 +0100

    Project id and user id not found exceptions passed to add project member handler

--- SprintManager.Application/Handlers/ProjectMembers/AddProjectMemberHandler.cs
+++ SprintManager.Application/Handlers/ProjectMembers/AddProjectMemberHandler.cs
@@ -5,25 +5,41 @@ using SprintManager.Application.DTOs;
 using SprintManager.Application.Exceptions;
 using SprintManager.Application.Interfaces;
 using SprintManager.Domain.Entities;
+using SprintManager.Exceptions.ExceptionsBase;
 
 namespace SprintManager.Application.Handlers.ProjectMembers
 {
     public class AddProjectMemberHandler : IRequestHandler<AddProjectMemberCommand, ProjectMemberDTO>
     {
         private readonly IProjectMemberRepository _projectMemberRepository;
+        private readonly IProjectRepository _projectRepository;
+        private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
 
-        public AddProjectMemberHandler(IProjectMemberRepository projectMemberRepository, IMapper mapper) 
+        public AddProjectMemberHandler(
+            IProjectMemberRepository projectMemberRepository,
+            IProjectRepository projectRepository,
+            IUserRepository userRepository,
+            IMapper mapper
+        ) 
         {
             _projectMemberRepository = projectMemberRepository;
+            _projectRepository = projectRepository;
+            _userRepository = userRepository;
             _mapper = mapper;
         }
 
         public async Task<ProjectMemberDTO> Handle(AddProjectMemberCommand request, CancellationToken cancellationToken)
         {
             var existingProjectMember = await _projectMemberRepository.GetByUserAndProjectIdAsync(request.UserId, request.ProjectId);
-
+            
             if (existingProjectMember != null) throw new SprintManagerConflictException($"A user with ID {request.UserId} is already assigned to a project with ID {request.ProjectId}.");
+
+            var project = await _projectRepository.GetByIdAsync(request.ProjectId);
+            var user = await _userRepository.GetByIdAsync(request.UserId);
+
+            if (project == null) throw new SprintManagerNotFoundException($"Project with ID {request.ProjectId} not found");
+            if (user == null) throw new SprintManagerNotFoundException($"User with ID {request.UserId} not found");
 
             var projectMember = new ProjectMember(request.ProjectId, request.UserId, request.Role);
             
--- SprintManager.Infrastructure/Repositories/ProjectMemberRepository.cs
+++ SprintManager.Infrastructure/Repositories/ProjectMemberRepository.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SprintManager.Application.Interfaces;
 using SprintManager.Domain.Entities;
-using SprintManager.Exceptions.ExceptionsBase;
 using SprintManager.Infrastructure.Data;
 
 namespace SprintManager.Infrastructure.Repositories
@@ -21,6 +20,7 @@ namespace SprintManager.Infrastructure.Repositories
                 .Include(pm => pm.Project)
                 .Include(pm => pm.User)
                 .OrderBy(pm => pm.Project)
+                .ThenBy(pm => pm.User.Name)
                 .ToListAsync();
         }
 
@@ -31,12 +31,6 @@ namespace SprintManager.Infrastructure.Repositories
 
         public async Task<ProjectMember?> GetByUserAndProjectIdAsync(Guid userId, Guid projectId)
         {
-            var user = await _context.Users.FindAsync(userId);
-            var project = await _context.Projects.FindAsync(projectId);
-
-            if (user == null) throw new SprintManagerNotFoundException($"User with ID {userId} not found");
-            if (project == null) throw new SprintManagerNotFoundException($"Project with ID {projectId} not found");
-
             return await _context.ProjectMembers
                 .FirstOrDefaultAsync(pm => pm.UserId == userId && pm.ProjectId == projectId);
         }
