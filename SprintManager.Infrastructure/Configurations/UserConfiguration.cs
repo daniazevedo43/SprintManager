75f15d4445a62aa37bec32b589ee7166f36b1bc2
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-08-27 18:48:24 +0100

    Unused using removed

--- SprintManager.Infrastructure/Configurations/UserConfiguration.cs
+++ SprintManager.Infrastructure/Configurations/UserConfiguration.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using SprintManager.Domain.Entities;
-using System.Reflection.Emit;
 
 namespace SprintManager.Infrastructure.Configurations
 {
