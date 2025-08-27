141e5efc14ed5c88826f480b8962bf4126bc3ed8
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-08-27 18:30:40 +0100

    Unused DTOs removed

--- SprintManager.Application/DTOs/RegisterDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SprintManager.Application.DTOs
-{
-    public class RegisterDTO
-    {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
