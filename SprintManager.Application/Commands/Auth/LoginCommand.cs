b776c1478488b40d7861b1a7aa41c32dfb190fcf
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-08-27 18:31:44 +0100

    Login command and handler

--- SprintManager.Application/Commands/Auth/LoginCommand.cs
+++ SprintManager.Application/Commands/Auth/LoginCommand.cs
@@ -3,7 +3,7 @@ using SprintManager.Application.DTOs;
 
 namespace SprintManager.Application.Commands.Auth
 {
-    public class LoginCommand : IRequest<UserDTO>
+    public class LoginCommand : IRequest<string>
     {
         public string Email { get; set; }
         public string Password { get; set; }
---
+++ SprintManager.Application/Handlers/Auth/LoginHandler.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using SprintManager.Application.Commands.Auth;
+using SprintManager.Application.DTOs;
+using SprintManager.Application.Interfaces;
+using SprintManager.Domain.Entities;
+using System.IdentityModel.Tokens.Jwt;
+using System.Reflection;
+
+namespace SprintManager.Application.Handlers.Auth
+{
+    public class LoginHandler : IRequestHandler<LoginCommand, string>
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly ITokenService _tokenService;
+
+        public LoginHandler(
+            UserManager<User> userManager,
+            ITokenService tokenService
+        )
+        {
+            _userManager = userManager;
+            _tokenService = tokenService;
+        }
+
+        public async Task<string> Handle(LoginCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByEmailAsync(request.Email);
+
+            var jwtToken = "";
+
+            if (user != null && await _userManager.CheckPasswordAsync(user, request.Password))
+            {
+                // Call service to create token
+                var token = _tokenService.CreateToken(user);
+                jwtToken = new JwtSecurityTokenHandler().WriteToken(token);
+            }
+
+            return jwtToken;
+        }
+    }
+}
