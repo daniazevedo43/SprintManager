09df2926a38c17d47da35839bde740e0c9bbdb9e
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-08-28 17:07:53 +0100

    Show username in work items endpoints

--- SprintManager.Application/Mappers/ApplicationMappingProfile.cs
+++ SprintManager.Application/Mappers/ApplicationMappingProfile.cs
@@ -13,11 +13,15 @@ namespace SprintManager.Application.Mappers
             CreateMap<ProjectMember, ProjectMemberDTO>()
                 // Maps User.UserName to ProjectMember.UserName in DTO, with null verification
                 .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.User != null ? src.User.UserName : null));
-            CreateMap<ProjectMember, ProjectMemberBasicDTO>();
+            CreateMap<ProjectMember, ProjectMemberBasicDTO>()
+                // Maps User.UserName to ProjectMemberBasicDTO.UserName in DTO, with null verification
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.User != null ? src.User.UserName : null));
             CreateMap<Sprint, SprintDTO>();
             CreateMap<WorkItem, WorkItemDTO>()
                 // Maps Sprint.SprintName to WorkItem.SprintName in DTO, with null verification
-                .ForMember(dest => dest.SprintName, opt => opt.MapFrom(src => src.Sprint != null ? src.Sprint.SprintName : null));
+                .ForMember(dest => dest.SprintName, opt => opt.MapFrom(src => src.Sprint != null ? src.Sprint.SprintName : null))
+                // Maps User.SprintName to WorkItem.SprintName in DTO, with null verification
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.User != null ? src.User.UserName : null));
             CreateMap<Comment, CommentDTO>()
                 // Maps WorkItem.WorkItemTitle to Comment.WorkItemTitle in DTO, with null verification
                 .ForMember(dest => dest.WorkItemTitle, opt => opt.MapFrom(src => src.WorkItem != null ? src.WorkItem.WorkItemTitle : null));
