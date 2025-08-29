a396b459f007bf88af03ae662c17905d86f04543
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-08-29 18:45:02 +0100

    Throw exception when username needs an uppercase letter, a number or a special character

--- SprintManager.Domain/Entities/User.cs
+++ SprintManager.Domain/Entities/User.cs
@@ -12,23 +12,33 @@ namespace SprintManager.Domain.Entities
         
         }
 
-        public User(string name, string userName, string email, string password) 
+        public User(string name, string username, string email, string password) 
         {
             if (string.IsNullOrWhiteSpace(name)) throw new ArgumentNullException(nameof(name), "Name can't be null or empty.");
-            if (string.IsNullOrWhiteSpace(userName)) throw new ArgumentNullException(nameof(userName), "Username can't be null or empty.");
+            if (string.IsNullOrWhiteSpace(username)) throw new ArgumentNullException(nameof(username), "Username can't be null or empty.");
             if (string.IsNullOrWhiteSpace(email)) throw new ArgumentNullException(nameof(email), "Email can't be null or empty.");
             if (string.IsNullOrWhiteSpace(password)) throw new ArgumentNullException(nameof(password), "Password can't be null or empty.");
 
-            if (userName.Contains(" ")) throw new SprintManagerInvalidUsernameException("Username can't have blank spaces.");
+            if (username.Contains(" ")) throw new SprintManagerInvalidUsernameException("Username can't have blank spaces.");
+            
+            if (!password.Any(char.IsUpper)) throw new SprintManagerPasswordRuleException("Password needs to have at least one uppercase letter.");
+            if (!password.Any(char.IsDigit)) throw new SprintManagerPasswordRuleException("Password needs to have at least one number.");
+            
+            var hasSpecialChar = password.Any(ch => !char.IsLetterOrDigit(ch));
+            
+            if (!hasSpecialChar)
+            {
+                throw new SprintManagerPasswordRuleException("Password needs to have at least one special character.");
+            }
 
             if (name.Length > 255) throw new SprintManagerTooLongException("Name is too long.", 255, name.Length, nameof(name));
-            if (userName.Length > 255) throw new SprintManagerTooLongException("UserName is too long.", 255, name.Length, nameof(name));
+            if (username.Length > 255) throw new SprintManagerTooLongException("UserName is too long.", 255, username.Length, nameof(username));
             if (email.Length > 255) throw new SprintManagerTooLongException("Email is too long.", 255, email.Length, nameof(email));
             if (password.Length < 12) throw new SprintManagerTooShortException("Password is too short.", 12, password.Length, nameof(password));
             if (password.Length > 64) throw new SprintManagerTooLongException("Password is too long.", 64, password.Length, nameof(password));
 
             Name = name;
-            UserName = userName;
+            UserName = username;
             Email = email;
         }
 
