8aae98415df894f276f3dde3da859665ce918625
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-08-29 15:49:32 +0100

    UserName added in GetAllUsersHandlerTests and GetUserByIdHandlerTests

--- SprintManager.Application.Tests/UserTests/GetAllUsersHandlerTests.cs
+++ SprintManager.Application.Tests/UserTests/GetAllUsersHandlerTests.cs
@@ -41,13 +41,15 @@ namespace SprintManager.Application.Tests.UserTests
                 new UserDTO 
                 { 
                     Id = users[0].Id,
-                    Name = users[0].Name, 
+                    Name = users[0].Name,
+                    UserName = users[0].UserName,
                     Email = users[0].Email 
                 },
                 new UserDTO
                 {
                     Id = users[1].Id,
                     Name = users[1].Name,
+                    UserName = users[0].UserName,
                     Email = users[1].Email
                 }
             };
@@ -64,6 +66,7 @@ namespace SprintManager.Application.Tests.UserTests
             {
                 Assert.Equal(usersDTOs[i].Id, result[i].Id);
                 Assert.Equal(usersDTOs[i].Name, result[i].Name);
+                Assert.Equal(usersDTOs[i].UserName, result[i].UserName);
                 Assert.Equal(usersDTOs[i].Email, result[i].Email);
             }
 
