bdf6cab9c4b093cf425a9e989577e84754d67d73
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-09-05 14:03:17 +0100

    Confirm email handler unit tests

---
+++ SprintManager.Application.Tests/AuthTests/ConfirmEmailHandlerTests.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using SprintManager.Application.Handlers.Auth;
+using SprintManager.Application.Queries.Auth;
+using SprintManager.Domain.Entities;
+using SprintManager.Exceptions.ExceptionsBase;
+
+namespace SprintManager.Application.Tests.AuthTests
+{
+    public class ConfirmEmailHandlerTests
+    {
+        private readonly Mock<UserManager<User>> _mockUserManager;
+        private readonly ConfirmEmailHandler _handler;
+
+        public ConfirmEmailHandlerTests()
+        {
+            // Create mocks for UserManager constructor's dependencies
+            var mockUserStore = new Mock<IUserStore<User>>();
+            var mockOptions = new Mock<IOptions<IdentityOptions>>();
+            var mockPasswordHasher = new Mock<IPasswordHasher<User>>();
+            var mockUserValidator = new List<IUserValidator<User>>
+            {
+                new Mock<IUserValidator<User>>().Object
+            };
+            var mockPasswordValidator = new List<IPasswordValidator<User>>
+            {
+                new Mock<IPasswordValidator<User>>().Object
+            };
+            var mockLookupNormalizer = new Mock<ILookupNormalizer>();
+            var mockErrors = new Mock<IdentityErrorDescriber>();
+            var mockServiceProvider = new Mock<IServiceProvider>();
+            var mockLogger = new Mock<ILogger<UserManager<User>>>();
+
+            // Initialize mock for each test
+            _mockUserManager = new Mock<UserManager<User>>(
+                mockUserStore.Object,
+                mockOptions.Object,
+                mockPasswordHasher.Object,
+                mockUserValidator,
+                mockPasswordValidator,
+                mockLookupNormalizer.Object,
+                mockErrors.Object,
+                mockServiceProvider.Object,
+                mockLogger.Object
+            );
+
+            _handler = new ConfirmEmailHandler(_mockUserManager.Object);
+        }
+
+        // Test handler - email confirmed with success
+        [Fact]
+        public async Task Handle_ConfirmsUserEmail()
+        {
+            var query = new ConfirmEmailQuery
+            {
+                UserId = Guid.NewGuid(),
+                Token = "testToken"
+            };
+
+            var user = new User("Daniel", "daniazevedo43", "d@gmail.com", "Abc123abc123!");
+
+            // Repository's mock configuration
+            _mockUserManager.Setup(r => r.FindByIdAsync(query.UserId.ToString())).ReturnsAsync(user);
+            _mockUserManager.Setup(r => r.ConfirmEmailAsync(user, query.Token)).ReturnsAsync(IdentityResult.Success);
+
+            await _handler.Handle(query, CancellationToken.None);
+
+            // Ensure FindByIdAsync was called exactly once.
+            _mockUserManager.Verify(r => r.FindByIdAsync(query.UserId.ToString()), Times.Once);
+
+            // Ensure ConfirmEmailAsync was called exactly once.
+            _mockUserManager.Verify(r => r.ConfirmEmailAsync(user, query.Token), Times.Once);
+        }
+
+        // Test exception throwing when a user is not found
+        [Fact]
+        public async Task VerifyUser_ThrowsException_WhenUserIsNotFound()
+        {
+            var query = new ConfirmEmailQuery
+            {
+                UserId = Guid.NewGuid(),
+                Token = "testToken"
+            };
+
+            // Repository's mock configuration
+            _mockUserManager.Setup(r => r.FindByIdAsync(query.UserId.ToString()));
+
+            var exception = await Assert.ThrowsAsync<SprintManagerNotFoundException>(
+                () => _handler.Handle(query, CancellationToken.None)
+            );
+
+            Assert.Equal($"User not found.", exception.Message);
+
+            // Ensure FindByIdAsync was called exactly once.
+            _mockUserManager.Verify(r => r.FindByIdAsync(query.UserId.ToString()), Times.Once);
+        }
+    }
+}
