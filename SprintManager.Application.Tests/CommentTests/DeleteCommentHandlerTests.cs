3a1b818444483714dffa71ff138fe4e2e91a3182
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-09-17 14:38:56 +0100

    Delete comment handler unit tests fixed

--- SprintManager.Application.Tests/CommentTests/DeleteCommentHandlerTests.cs
+++ SprintManager.Application.Tests/CommentTests/DeleteCommentHandlerTests.cs
@@ -1,24 +1,31 @@
-﻿using Moq;
+﻿using Microsoft.AspNetCore.Http;
+using Moq;
 using SprintManager.Application.Commands.Comments;
 using SprintManager.Application.Handlers.Comments;
 using SprintManager.Application.Interfaces;
 using SprintManager.Domain.Entities;
 using SprintManager.Exceptions.ExceptionsBase;
+using System.Security.Claims;
 
 namespace SprintManager.Application.Tests.CommentTests
 {
     public class DeleteCommentHandlerTests
     {
+        private readonly Mock<IHttpContextAccessor> _mockHttpContextAccessor;
         private readonly Mock<ICommentRepository> _mockCommentRepository;
         private readonly DeleteCommentHandler _handler;
 
         public DeleteCommentHandlerTests() 
         {
             // Initialize mock for each test
+            _mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
             _mockCommentRepository = new Mock<ICommentRepository>();
 
             // Initialize handler injecting the mock
-            _handler = new DeleteCommentHandler(_mockCommentRepository.Object);
+            _handler = new DeleteCommentHandler(
+                _mockHttpContextAccessor.Object,
+                _mockCommentRepository.Object
+            );
         }
 
         // Test handler
@@ -30,19 +37,54 @@ namespace SprintManager.Application.Tests.CommentTests
                 Id = Guid.NewGuid(),
             };
 
-            var comment = new Comment(Guid.NewGuid(), Guid.NewGuid(), "Test comment");
+            var userId = Guid.NewGuid();
 
-            // Repository's mock configuration
+            var comment = new Comment(Guid.NewGuid(), userId, "Test comment");
+
+            _mockHttpContextAccessor
+                .Setup(a => a.HttpContext.User
+                .FindFirst(ClaimTypes.NameIdentifier))
+                .Returns(new Claim(ClaimTypes.NameIdentifier, userId.ToString()));
+
             _mockCommentRepository.Setup(r => r.GetByIdAsync(command.Id)).ReturnsAsync(comment);
             _mockCommentRepository.Setup(r => r.DeleteAsync(comment));
 
             await _handler.Handle(command, CancellationToken.None);
+
+            // Ensure HttpContextAccesor was used exactly once.
+            _mockHttpContextAccessor
+                .Verify(a => a.HttpContext.User
+                .FindFirst(ClaimTypes.NameIdentifier), Times.Once);
 
             // Ensure GetByIdAsync was called exactly once with the correct ID.
             _mockCommentRepository.Verify(r => r.GetByIdAsync(command.Id), Times.Once);
 
             // Ensure DeleteAsync was called exactly once.
             _mockCommentRepository.Verify(r => r.DeleteAsync(comment), Times.Once);
+        }
+
+        // Test exception throwing when user is not authenticated
+        [Fact]
+        public async Task VerifyUser_ThrowException_WhenAuthenticatedUserIsNotFound()
+        {
+            var command = new DeleteCommentCommand
+            {
+                Id = Guid.NewGuid(),
+            };
+
+            _mockHttpContextAccessor
+                .Setup(r => r.HttpContext.User
+                .FindFirst(ClaimTypes.NameIdentifier));
+
+            var exception = await Assert.ThrowsAsync<UnauthorizedAccessException>(
+                () => _handler.Handle(command, CancellationToken.None)
+            );
+
+            Assert.Equal($"User not authenticated.", exception.Message);
+
+            // Ensure HttpContextAccesor was used exactly once.
+            _mockHttpContextAccessor
+                .Verify(r => r.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier), Times.Once);
         }
 
         // Test exception throwing when comment is not found
@@ -54,7 +96,13 @@ namespace SprintManager.Application.Tests.CommentTests
                 Id = Guid.NewGuid(),
             };
 
-            // Repository's mock configuration
+            var userId = Guid.NewGuid();
+
+            _mockHttpContextAccessor
+                .Setup(a => a.HttpContext.User
+                .FindFirst(ClaimTypes.NameIdentifier))
+                .Returns(new Claim(ClaimTypes.NameIdentifier, userId.ToString()));
+
             _mockCommentRepository.Setup(r => r.GetByIdAsync(command.Id));
 
             var exception = await Assert.ThrowsAsync<SprintManagerNotFoundException>(
@@ -62,6 +110,44 @@ namespace SprintManager.Application.Tests.CommentTests
             );
 
             Assert.Equal($"Comment with ID {command.Id} not found.", exception.Message);
+
+            // Ensure HttpContextAccesor was used exactly once.
+            _mockHttpContextAccessor
+                .Verify(a => a.HttpContext.User
+                .FindFirst(ClaimTypes.NameIdentifier), Times.Once);
+
+            // Ensure GetByIdAsync was called exactly once.
+            _mockCommentRepository.Verify(r => r.GetByIdAsync(command.Id), Times.Once);
+        }
+
+        // Test exception throwing when comment was not made by authenticated user
+        [Fact]
+        public async Task VerifyComment_ThrowsException_WhenCommentWasNotMadeByAuthenticatedUser()
+        {
+            var command = new DeleteCommentCommand
+            {
+                Id = Guid.NewGuid(),
+            };
+
+            var comment = new Comment(Guid.NewGuid(), Guid.NewGuid(), "Test comment");
+
+            _mockHttpContextAccessor
+                .Setup(a => a.HttpContext.User
+                .FindFirst(ClaimTypes.NameIdentifier))
+                .Returns(new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()));
+
+            _mockCommentRepository.Setup(r => r.GetByIdAsync(command.Id)).ReturnsAsync(comment);
+
+            var exception = await Assert.ThrowsAsync<UnauthorizedAccessException>(
+                () => _handler.Handle(command, CancellationToken.None)
+            );
+
+            Assert.Equal($"You can't delete comments made by other users.", exception.Message);
+
+            // Ensure HttpContextAccesor was used exactly once.
+            _mockHttpContextAccessor
+                .Verify(a => a.HttpContext.User
+                .FindFirst(ClaimTypes.NameIdentifier), Times.Once);
 
             // Ensure GetByIdAsync was called exactly once.
             _mockCommentRepository.Verify(r => r.GetByIdAsync(command.Id), Times.Once);
