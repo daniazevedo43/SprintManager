e0425bd82bc0953bc3bf0371e0385d86402b5511
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-10-14 14:24:35 +0100

    RegisterRequest interface created

--- SprintManager.Frontend/src/app/register/page.tsx
+++ SprintManager.Frontend/src/app/register/page.tsx
@@ -1,21 +1,24 @@
 "use client"
 
 import React from 'react';
+import { RegisterRequest } from '../types/auth';
 
 export default function Register() {
 
     const postData = async () => {
+        const request: RegisterRequest = {
+            name: 'Daniel Azevedo',
+            userName: "daniazevedo97",
+            email: "daniazevedo685@gmail.com",
+            password: "Abc123abc123!"
+        }
+
         const response = await fetch('https://localhost:7060/api/Auth/register', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({
-                name: 'Daniel Azevedo',
-                userName: "daniazevedo97",
-                email: "daniazevedo685@gmail.com",
-                password: "Abc123abc123!"
-            }),
+            body: JSON.stringify(request),
         });
 
             const result = await response.json();
---
+++ SprintManager.Frontend/src/app/types/auth.ts
@@ -0,0 +1,6 @@
+export interface RegisterRequest {
+  name: string,
+  userName: string,
+  email: string,
+  password: string
+}
