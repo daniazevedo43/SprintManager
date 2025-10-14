0ca941cb11df0e5aad4fc83eaa580a5b0613e813
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-10-14 18:07:00 +0100

    Register form created

--- SprintManager.Frontend/src/app/register/page.tsx
+++ SprintManager.Frontend/src/app/register/page.tsx
@@ -1,16 +1,22 @@
 "use client"
 
-import React from 'react';
+import React, { useState } from 'react';
+import Form from 'next/form'
 import { RegisterRequest } from '../types/auth';
 
 export default function Register() {
 
-    const postData = async () => {
+    const [name, setName] = useState("");
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const registerData = async () => {
         const request: RegisterRequest = {
-            name: 'Daniel Azevedo',
-            userName: "daniazevedo97",
-            email: "daniazevedo685@gmail.com",
-            password: "Abc123abc123!"
+            name: name,
+            username: username,
+            email: email,
+            password: password
         }
 
         const response = await fetch('https://localhost:7060/api/Auth/register', {
@@ -25,9 +31,45 @@ export default function Register() {
             console.log(result);
     };
 
+    console.log(name);
+    console.log(username);
+    console.log(email);
+    console.log(password);
+
     return (
-        <div>
-            <button onClick={postData} type="submit">Submit</button>
+        <div className='flex min-h-screen justify-center items-center'>
+            <Form style={{border: '1px solid red'}} className='flex flex-col w-125' action="">
+                <label>Name:</label>
+                <input 
+                    className='border black' 
+                    name="name" 
+                    onChange={(event) => setName(event.target.value)}
+                />
+                
+                <label>Username:</label>
+                <input 
+                    className='border black' 
+                    name="username" 
+                    onChange={(event) => setUsername(event.target.value)}
+                />
+
+                <label>Email:</label>
+                <input 
+                    className='border black' 
+                    name="email"
+                    onChange={(event) => setEmail(event.target.value)}
+                />
+
+                <label>Password:</label>
+                <input 
+                    className='border black' 
+                    name="password" 
+                    type='password'
+                    onChange={(event) => setPassword(event.target.value)}
+                />
+                
+                <button onClick={registerData} type="submit">Submit</button>
+            </Form>
         </div>
     )
 }
